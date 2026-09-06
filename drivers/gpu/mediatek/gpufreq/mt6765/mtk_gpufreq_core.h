1a92f0fa810bfc450c53a4ed0af7c05025a08a1a
JBofficial-tech <aimstickplayz@gmail.com>
2026-09-06 14:42:22 +0530

    Update mtk_gpufreq_core.h

--- drivers/gpu/mediatek/gpufreq/mt6765/mtk_gpufreq_core.h
+++ drivers/gpu/mediatek/gpufreq/mt6765/mtk_gpufreq_core.h
@@ -30,20 +30,20 @@
 #define SEG2_GPU_DVFS_VSRAM1			(87500)		/* mV x 100 */
 #define SEG2_GPU_DVFS_VSRAM2			(87500)		/* mV x 100 */
 
-/**************************************************
+/*******************************************
  * MT6765 segment_3 : GPU DVFS OPP table Setting
- **************************************************/
-#define SEG2_GPU_DVFS_FREQ0			(680000)	/* KHz */
-#define SEG2_GPU_DVFS_FREQ1			(520000)	/* KHz */
-#define SEG2_GPU_DVFS_FREQ2			(416000)	/* KHz */
+ *******************************************/
+#define SEG3_GPU_DVFS_FREQ0        (680000)    /* KHz */
+#define SEG3_GPU_DVFS_FREQ1        (520000)    /* KHz */
+#define SEG3_GPU_DVFS_FREQ2        (416000)    /* KHz */
 
-#define SEG2_GPU_DVFS_VOLT0			(100000)		/* mV x 100 */
-#define SEG2_GPU_DVFS_VOLT1			(85000)		/* mV x 100 */
-#define SEG2_GPU_DVFS_VOLT2			(75000)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VOLT0        (100000)    /* mV x 100 */
+#define SEG3_GPU_DVFS_VOLT1        (85000)     /* mV x 100 */
+#define SEG3_GPU_DVFS_VOLT2        (75000)     /* mV x 100 */
 
-#define SEG2_GPU_DVFS_VSRAM0			(105000)		/* mV x 100 */
-#define SEG2_GPU_DVFS_VSRAM1			(87500)		/* mV x 100 */
-#define SEG2_GPU_DVFS_VSRAM2			(87500)		/* mV x 100 */
+#define SEG3_GPU_DVFS_VSRAM0       (105000)    /* mV x 100 */
+#define SEG3_GPU_DVFS_VSRAM1       (87500)     /* mV x 100 */
+#define SEG3_GPU_DVFS_VSRAM2       (87500)     /* mV x 100 */
 
 /**************************************************
  * MT6765T segment_4 : GPU DVFS OPP table Setting
